61b9e5fadbd39c7575d343236acdb24b8de4081b
Siddharth Mistry <mistry.siddharth@gmail.com>
2017-09-24 19:11:52 -0400

    updated test case for loadFeed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,16 +3,15 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -31,8 +30,8 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('URLs are defined and are not empty', function() {
-            for(var i in allFeeds) {
+        it('URLs are defined and are not empty', function() {
+            for (var i in allFeeds) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url).not.toBe('');
             }
@@ -43,7 +42,7 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it('feeds have a name and are not empty', function() {
+        it('feeds have a name and are not empty', function() {
             for (var i in allFeeds) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name).not.toBe('');
@@ -54,30 +53,30 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
-        
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        
+
         it('hide menu by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-        
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-        
+
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+
         it('menu changes visibility on click', function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
-    });  
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
@@ -89,22 +88,22 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-         beforeEach(function(done) {
+        beforeEach(function(done) {
             loadFeed(0, function() {
                 done();
-            });    
-         });
-         
-         //check if there is at least one entry
-         it('there is at least one entry', function() {
-            var entryIndex = $('.entry').length;
+            });
+        });
+
+        //check if there is at least one entry
+        it('there is at least one entry', function() {
+            var entryIndex = $('.feed .entry').length;
             expect(entryIndex).toBeGreaterThan(0);
-         });
+        });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        
+
         var previousFeed;
 
         /* TODO: Write a test that ensures when a new feed is loaded
